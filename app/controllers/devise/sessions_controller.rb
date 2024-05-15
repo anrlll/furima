9c1ce36570dd3b40c10433af833de63660f308aa
Shinya Yamamoto <shinya.yamamoto6@persol.co.jp>
2024-05-15 16:34:32 +0900

    controll modify

--- app/controllers/devise/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < ApplicationController
-  def new
-    redirect_to new_user_session_path
-  end
-
-  def desrtoy
-    redirect_to desrtoy_user_session_parh
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
-class UsersController < ApplicationController
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  def index
-  end
+# class UsersController < ApplicationController
+#   before_action :configure_permitted_parameters, if: :devise_controller?
+#   def index
+#   end
 
-  private
+#   private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :password, :date_birth, :last_name, :first_name, :last_name_kana, :first_name_kana])
-  end
-end
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :password, :date_birth, :last_name, :first_name, :last_name_kana, :first_name_kana])
+#   end
+# end
