cf6e1b32813540f050a45478ea113ee43f345130
anrlll <anriii.3939@gmail.com>
2024-05-15 13:06:39 +0900

    add factory

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe "商品出品機能" do
     it "商品画像が空では投稿できない" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
     end
     it "商品名が空では投稿できない" do
     end
