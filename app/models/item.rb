37a4a056d314706d5551b16a4b16b765490c01e8
anrlll <anriii.3939@gmail.com>
2024-05-15 10:15:53 +0900

    add validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
 
-  # validates :title, :text, presence: true
-  
-  validates :category_id, :condition_id, :costdelivery_id, :regiondelivery_id, :datedelivery_id, numericality: { other_than: 1 , message:"can't be blank" } 
+  validates :image, presence: true
+
+  validates :name, length: { in: 1..40 }, presence: true
+
+  validates :description, length: { in: 1..1000 },presence: true
+
+  validates :price, numericality: {
+    only_integer: true, greather_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
+  }
+
+  validates :category_id, :condition_id, :costdelivery_id, :regiondelivery_id, :datedelivery_id, numericality: { other_than: 1, message:"can't be blank" }, presence: true
 end
