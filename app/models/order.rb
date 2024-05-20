7bc6244f6c938becd67aa69cd1ad4deced8ff2fc
Shinya Yamamoto <shinya.yamamoto6@persol.co.jp>
2024-05-21 04:33:55 +0900

    address validation added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :postcode, presence: true
+  validates :postcode, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/}
   validates :regiondelivery_id, numericality: { other_than: 1 }
   validates :municipalities, presence: true
   validates :blocknumber, presence: true
-  validates :telnumber, presence: true
+  validates :telnumber, presence: true, format: {with: /\A\d{10,11}\z/}
   validates :item, presence: true
   attr_accessor :token
   validates :token, presence: true
